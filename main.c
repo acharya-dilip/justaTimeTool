7e84d1779fc42427b45a538a0fc7df9cc72be86e
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-12-05 13:36:52 +0545

    init of windowMain

--- main.c
+++ main.c
@@ -7,7 +7,11 @@ struct windowMain {
 }windowMain;
 static void activate(GtkApplication *app, gpointer user_data) {
 
-
+    //Init of windowMain
+    windowMain.windowMain = gtk_application_window_new(app);
+    gtk_window_set_default_size(GTK_WINDOW(windowMain.windowMain),400,400);
+    gtk_window_set_title(GTK_WINDOW(windowMain.windowMain),"JustaTimeTool");
+    gtk_window_present(GTK_WINDOW(windowMain.windowMain));
 
 }
 
